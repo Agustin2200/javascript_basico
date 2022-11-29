49a5e19d3aaa133a8626e5250a6370d666af2309
Agustin2200 <118233589+Agustin2200@users.noreply.github.com>
2022-11-28 23:22:19 -0300

    Add files via upload

---
+++ practica_jsbasico_AcevedoAgustin.js
@@ -0,0 +1,269 @@
+// EJERCICIOS DE TAREA "JAVASCRIPT BASICO"
+
+//EJERCICIO 1 - COMENTARIOS 
+
+// Comentario de tipo 1
+
+/*
+  Comentario de tipo 2
+*/
+
+
+//EJERCICIO 2 
+var myName;
+
+//EJERCICIO 3 
+
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
+
+//EJERCICIO 4 
+ 
+// Setup
+var a;
+a = 7;
+var b;
+// Only change code below this line
+
+b = a;
+
+//EJERCICO 5 
+var a = 9;
+
+/*
+EJERCICIO 6: Cree dos nuevas variables de cadena:
+ myFirstNamey myLastNameasígneles los valores de su nombre y apellido, respectivamente.
+*/
+
+var myFirstName = "Agustin"
+var myLastName = "Acevedo"
+
+/*
+EJERCICIO 7: Inicialice las tres variables a, b, y c con 5, 10 y "I am a"
+respectivamente para que no sean undefined.
+*/
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/*
+EJERCICIO 8: Modifique las declaraciones y asignaciones existentes para que sus nombres usen camelCase .
+
+No cree ninguna variable nueva.
+*/
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/*
+EJERCICIO 9: Actualice el código para que solo use la letpalabra clave.
+*/
+var catName = "Oliver";
+var catSound = "Meow!";
+
+/*
+EJERCICIO 10: Cambie el código para que todas las variables se declaren usando leto const. 
+Úselo letcuando desee que la variable cambie y constcuando desee que la variable permanezca constante. 
+Además, cambie el nombre de las variables 
+declaradas con constpara ajustarse a las prácticas comunes. No cambie las cadenas asignadas a las variables.
+*/
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+/*
+EJERCICIO 11: Cambie el 0 para que la suma sea igual a 20.
+*/
+
+const sum = 10 + 10;
+
+/*
+EJERCICIO 12: Cambie el 0para que la diferencia sea 12.
+*/
+const difference = 45 - 33;
+/*
+ EJERCICIO 13: Cambie el 0para que el producto sea igual a 80.
+*/
+ const product = 8 * 10;
+
+/*
+ EJERCICIO 14: Cambia el 0para que quotientsea igual a 2.
+*/
+const quotient = 66 / 33;
+
+/*
+EJERCICIO 15: Cambie el código para usar el ++ operador en myVar.
+*/
+let myVar = 87;
+// Only change code below this line
+myVar ++;
+
+/*
+EJERCICIO 16: Cambie el código para usar el -- operador en myVar.
+*/
+let myVar2 = 11;
+// Only change code below this line
+myVar2 --;
+
+/*
+EJERCICIO 17: 
+*/
+const ourDecimal = 5.7;
+// Only change code below this line
+const myDecimal = 5.7
+
+/*
+ejercicio 18
+*/
+const product2 = 1.0 * 5.0;
+//EJERCICIO 19 
+const quotient2 = 4.4 / 2.0; // Change this line
+
+//EJERCICIO 20 
+const remainder = 11 % 3;
+
+
+//EJERCICIO 21
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//EJERCICIO 22 
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+// EJERCICIO 23 
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+//EJERCICIO 24 
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//EJERCICIO 25 
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//EJERCICIO 26 
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//EJERCICIO 27 
+const myStr3 = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//EJERCICIO 28 
+const myStr4 = "This is the start. " + "This is the end."; // Change this line
+
+//ejercicio 29
+
+let myStr5 = "This is the first sentence. ";
+myStr5 += "This id the second sentence.";
+
+//EJERCICIO 30
+// Only change code below this line
+const myName = "Acevedo Agustin";
+const myStr6 = "My name is " + myName + " and I am well!";
+
+//EJERCICIO 31 
+// Change code below this line
+const someAdjective = "very good";
+let myStr7 = "Learning to code is ";
+
+myStr += someAdjective
+
+//EJERCICIO 32 
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//EJERCICIO 33
+// Setup
+let firstLetterOfLastName = "";
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//EJERCICIO 34
+// Setup
+let myStr8 = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//EJERCICIO 35 
+// Setup
+const lastName3 = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//EJERCICIO 36
+// Setup
+const lastName4 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//EJERCICIO 37
+// Setup
+const lastName5 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//EJERCICIO 38 
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " in the square"  ; // Change this line
+// Only change code above this line
+
+//EJERCICIO 39
+// Only change code below this line
+const myArray = ["Agustin", 1];
+
+//EJERCICIO 40
+// Only change code below this line
+const myArray2 = [["Lakers", 23], ["Warriors", 30]];
+
